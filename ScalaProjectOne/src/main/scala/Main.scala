0f5d46bb12df85d02c0fa6c17733e4cc944f2da5
Balajihari3105 <balaji2000d@gmail.com>
2023-12-15 10:42:12 +0530

    the changes made 123

--- ScalaProjectOne/src/main/scala/Main.scala
+++ ScalaProjectOne/src/main/scala/Main.scala
@@ -1,5 +1,16 @@
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
+def add(n1:Int,n2:Int)={
+  n1+n2
+}
+    println(add(10,200))
   }
+  def mul(mul1:Int,mul2:Int)={
+    mul2*mul1
+  }
+  def div(mul1:Int,mul2:Int)={
+    mul2/mul1
+  }
+
 }
