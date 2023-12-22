90edb4f12af21ed6f22b25c0c686206ed147394b
Balajihari3105 <balaji2000d@gmail.com>
2023-12-22 08:41:04 +0530

    testcase for fibonacci modified

--- ScalaProjectOne/src/test/scala/FibonaciSeriesSpec.scala
+++ ScalaProjectOne/src/test/scala/FibonaciSeriesSpec.scala
@@ -5,21 +5,21 @@ import java.util.Collections.list
 
 class FibonacciSeriesSpec extends AnyFunSuite {
 
-  test("Fibonacci series for 0 should be empty") {
+  test("Fibonacci series for with ussual function") {
 
     assert( FibonaciSeries.fibonacciRecursive(0) ===0)
     assert( FibonaciSeries.fibonacciRecursive(1) ===1)
     assert( FibonaciSeries.fibonacciRecursive(2) ===1)
     assert( FibonaciSeries.fibonacciRecursive(3) ===2)
   }
-  test("Fibonacci series for 5 should be empty") {
+  test("Fibonacci series using recursive") {
     assert( FibonaciSeries.fibonacciRecursive(4) ===3)
     assert(FibonaciSeries.fibonacciRecursive(5) === 5)
     assert( FibonaciSeries.fibonacciRecursive(10) ===55)
   }
 
   //test case for tail recursion
-  test("Fibonacci series using tail recursive should be empty") {
+  test("Fibonacci series using tail recursive") {
     val result = FibonaciSeries.fibTailRec(10)
     val expectedList = List(0, 1, 1, 2, 3, 5, 8, 13, 21, 34)
 
